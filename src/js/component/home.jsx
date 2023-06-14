910f6c6bb01abd2cb3dc7585037cfe3477fa7d59
Adriano Schneider Maximiano <adriano-sm@hotmail.com>
2023-06-15 00:35:43 +0100

    Counter
    React page with a counter that goes from 0000 till 9999.

--- src/js/component/home.jsx
+++ src/js/component/home.jsx
@@ -2,25 +2,53 @@ import React from "react";
 
 //include images into your bundle
 import rigoImage from "../../img/rigo-baby.jpg";
+import clock from "../../img/clock-regular.svg";
 
 //create your first component
 const Home = () => {
-	return (
-		<div className="text-center">
-			<h1 className="text-center mt-5">Hello Rigo!</h1>
-			<p>
-				<img src={rigoImage} />
-			</p>
-			<a href="#" className="btn btn-success">
-				If you see this green button... bootstrap is working...
-			</a>
-			<p>
-				Made by{" "}
-				<a href="http://www.4geeksacademy.com">4Geeks Academy</a>, with
-				love!
-			</p>
-		</div>
-	);
+  return (
+    <div className="text-center">
+      <h1 className="text-center mt-5">Simple counter</h1>
+      <p>
+        <img src={rigoImage} />
+      </p>
+      <div className="container d-flex flex-row bg-dark">
+        <div
+          className="container bg-dark text-white"
+          style={{ fontSize: "75px", fontFamily: "calibri" }}
+        >
+          <i class="fa-regular fa-clock"></i>
+        </div>
+        <div
+          className="container bg-dark text-white"
+          style={{ fontSize: "75px", fontFamily: "calibri" }}
+        >
+          0
+        </div>
+        <div
+          className="container bg-dark text-white"
+          style={{ fontSize: "75px", fontFamily: "calibri" }}
+        >
+          0
+        </div>
+        <div
+          className="container bg-dark text-white"
+          style={{ fontSize: "75px", fontFamily: "calibri" }}
+        >
+          0
+        </div>
+        <div
+          className="container bg-dark text-white"
+          style={{ fontSize: "75px", fontFamily: "calibri" }}
+        >
+          0
+        </div>
+      </div>
+      <p className="mt-5 fs-1">
+        This is a simple counter, it increase each second.
+      </p>
+    </div>
+  );
 };
 
 export default Home;
