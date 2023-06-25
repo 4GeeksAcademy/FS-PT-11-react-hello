5e55e7021d9892bc43da582a61a119d112edf70e
Adriano Schneider Maximiano <adriano-sm@hotmail.com>
2023-06-25 02:14:35 +0100

    finishing the code to counter the time every 1 sec

--- src/js/component/home.jsx
+++ src/js/component/home.jsx
@@ -1,54 +1,64 @@
-import React from "react";
+// import React from "react";
 
-//include images into your bundle
-import rigoImage from "../../img/rigo-baby.jpg";
-import clock from "../../img/clock-regular.svg";
+// //include images into your bundle
+// import rigoImage from "../../img/rigo-baby.jpg";
+// import clock from "../../img/clock-regular.svg";
 
-//create your first component
-const Home = () => {
-  return (
-    <div className="text-center">
-      <h1 className="text-center mt-5">Simple counter</h1>
-      <p>
-        <img src={rigoImage} />
-      </p>
-      <div className="container d-flex flex-row bg-dark">
-        <div
-          className="container bg-dark text-white"
-          style={{ fontSize: "75px", fontFamily: "calibri" }}
-        >
-          <i class="fa-regular fa-clock"></i>
-        </div>
-        <div
-          className="container bg-dark text-white"
-          style={{ fontSize: "75px", fontFamily: "calibri" }}
-        >
-          0
-        </div>
-        <div
-          className="container bg-dark text-white"
-          style={{ fontSize: "75px", fontFamily: "calibri" }}
-        >
-          0
-        </div>
-        <div
-          className="container bg-dark text-white"
-          style={{ fontSize: "75px", fontFamily: "calibri" }}
-        >
-          0
-        </div>
-        <div
-          className="container bg-dark text-white"
-          style={{ fontSize: "75px", fontFamily: "calibri" }}
-        >
-          0
-        </div>
-      </div>
-      <p className="mt-5 fs-1">
-        This is a simple counter, it increase each second.
-      </p>
-    </div>
-  );
-};
+// //create your first component
+// const Home = (props) => {
+//   const counter = 0;
+//   return (
+//     <div className="text-center">
+//       <h1 className="text-center mt-5">Simple counter</h1>
+//       <p>
+//         <img src={rigoImage} />
+//       </p>
+//       <div className="container d-flex flex-row bg-dark">
+//         <div
+//           className="container bg-dark text-white"
+//           style={{ fontSize: "75px", fontFamily: "calibri" }}
+//         >
+//           <i class="far fa-clock"></i>
+//         </div>
+//         <div
+//           className="container bg-dark text-white"
+//           style={{ fontSize: "75px", fontFamily: "calibri" }}
+//         >
+//           {props.four}
+//         </div>
+//         <div
+//           className="container bg-dark text-white"
+//           style={{ fontSize: "75px", fontFamily: "calibri" }}
+//         >
+//           {props.three}
+//         </div>
+//         <div
+//           className="container bg-dark text-white"
+//           style={{ fontSize: "75px", fontFamily: "calibri" }}
+//         >
+//           {props.twoo}
+//         </div>
+//         <div
+//           className="container bg-dark text-white"
+//           style={{ fontSize: "75px", fontFamily: "calibri" }}
+//         >
+//           {props.one}
+//         </div>
+//       </div>
+//       <p className="mt-5 fs-1">
+//         This is a simple counter, it increase every second.
+//       </p>
+//     </div>
+//   );
+// };
+// let counter = 0;
+// setInterval(function() => {
+// 	const numberFour = Math.floor(counter/10000);
+// 	const NumberThree = Math.floor(counter/1000);
+// 	const NumberTwo = Math.floor(counter/100);
+// 	const numberOne = Math.floor(counter/10);
+// 	console.log(numberOne, NumberTwo, NumberThree, numberFour);;
+// 	<Home, one={counter.indexOf()} />
+// }, 1000);
 
-export default Home;
+// export default Home;
